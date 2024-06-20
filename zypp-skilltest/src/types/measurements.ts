30d38b65b188ccd69d49f0c8f32ae8337702f114
Dabronzo <ayrtonterceiro@gmail.com>
2024-06-20 10:57:17 +0200

    added some types

---
+++ zypp-skilltest/src/types/measurements.ts
@@ -0,0 +1,52 @@
+import { z } from 'zod';
+
+
+const measurementSchema = z.object({
+    id: z.string(),
+    timestamp: z.string().optional(),
+    temperature: z.number().optional(),
+    groundtemperature: z.number().optional(),
+    feeltemperature: z.number().optional(),
+    windgusts: z.number().optional(),
+    windspeedBft: z.number().optional(),
+    humidity: z.number().optional(),
+    precipitation: z.number().optional(),
+    sunpower: z.number().optional(),
+    stationid: z.number(),
+});
+
+const stationSchema = z.object({
+    id: z.string(),
+    stationid: z.number(),
+    stationname: z.string().optional(),
+    lat: z.number(),
+    lon: z.number(),
+    regio: z.string(),
+    measurements: measurementSchema
+});
+
+const buienradarAPISchema = z.object({
+    id: z.string(),
+    timestamp: z.string().optional(),
+    temperature: z.number().optional(),
+    groundtemperature: z.number().optional(),
+    feeltemperature: z.number().optional(),
+    windgusts: z.number().optional(),
+    windspeedBft: z.number().optional(),
+    humidity: z.number().optional(),
+    precipitation: z.number().optional(),
+    sunpower: z.number().optional(),
+    stationid: z.number(),
+    stationname: z.string().optional(),
+    lat: z.number(),
+    lon: z.number(),
+    regio: z.string(),
+    measurements: measurementSchema
+})
+
+
+
+
+export type Measurement = z.infer<typeof measurementSchema>;
+export type Station = z.infer<typeof stationSchema>;
+export type BuienradarAPI = z.infer<typeof buienradarAPISchema>;
